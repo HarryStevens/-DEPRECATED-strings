0e7238a6756f5e6a585cf8ebe3f9c9d101fcbca8
HarryStevens <harryjosephstevens@gmail.com>
2017-01-18 17:51:05 +0530

    Add utility function for stripping commas from number strings

--- strings.js
+++ strings.js
@@ -10,10 +10,10 @@ var strings = {
     return x.toString().charAt(0).toUpperCase() + x.slice(x.length-(x.length-1));
   },
   numberCommas: function(x){
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return stripCommas(x).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   },
   numberLakhs: function(x){
-    x = x.toString();
+    x = stripCommas(x);
     var afterPoint = x.indexOf(".") > 0 ? x.substring(x.indexOf("."), x.length) : "";
     x = Math.floor(x).toString();
     var lastThree = x.substring(x.length - 3), otherNumbers = x.substring(0, x.length - 3);
@@ -23,10 +23,23 @@ var strings = {
   numberHundredths: function(x){
     var s = x.toString().split("."),
       a = s[0],
-      b = s[1];
-    var c = b == undefined ? "00" : b.length == 1 ? "0" : b.length > 2 ? Math.round(b / Math.pow(10, b.length - 2)) : x;
+      b = s[1],
+      c;
+
+    if (b == undefined) {
+      c = "00";
+    } else if (b.length == 1){
+      c = b + "0"
+    } else if (b.length == 2){
+      c = b;
+    } else if (b.length > 2){
+      c = Math.round(b / Math.pow(10, b.length - 2))
+    } else {
+      c = "00"
+    }
+
     if (c == 100) {
-      return a.replace(/,/g , "") + 1 + ".00";
+      return stripCommas(a) + 1 + ".00";
     } else {
       return a + "." + c;
     }
@@ -35,9 +48,9 @@ var strings = {
     var s = x.toString().split("."),
       a = s[0],
       b = s[1];
-    var c = b == undefined ? 0 : b.length == 1 ? b : b.length > 1 ? Math.round(b / Math.pow(10, b.length - 1)) : x;
+    var c = b == undefined ? 0 : b.length == 1 ? b : b.length > 1 ? Math.round(b / Math.pow(10, b.length - 1)) : 0;
     if (c == 10) {
-      return a.replace(/,/g , "") + 1 + ".0";
+      return stripCommas(a) + 1 + ".0";
     } else {
       return a + "." + c;
     }
@@ -49,3 +62,8 @@ var strings = {
     return x.toString().split(" ").reverse().join(" ");
   }
 };
+
+// utility functions
+function stripCommas(x){
+  return x.toString().replace(/,/g , "");
+}
