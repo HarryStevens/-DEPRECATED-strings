1b940498a6ea2b39962150a7a77fc1ff039904c3
HarryStevens <harryjosephstevens@gmail.com>
2017-02-04 15:46:55 +0530

    Use double quotes

--- src/strings.js
+++ src/strings.js
@@ -67,11 +67,11 @@ window.strings = (function () {
 
   o.removeTags = function(input, allowed){
     allowed = allowed.map(function(d){ return "<" + d + ">"; }).join(",");
-    allowed = (((allowed || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join(''); // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
+    allowed = (((allowed || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join(""); // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
     var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
         commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
     return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
-        return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+        return allowed.indexOf("<" + $1.toLowerCase() + ">") > -1 ? $0 : "";
     });
   }
 
